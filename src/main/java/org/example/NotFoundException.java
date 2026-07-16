e08a8ba37042e60f5bd24bdae8a0b54b0d8e2caa
Ulugbek2080 <ulugbekabduzamilov@gmail.com>
2026-07-16 15:16:38 +0500

    yml

--- src/main/java/org/example/NotFoundException.java
+++ src/main/java/org/example/NotFoundException.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String s) {
+        super(s);
+    }
+    public static Product findById(Product[] products, int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static void removeById(ShopRepository shop, int id) {
+        if (findById((shop.findAll()), id) == null){
+            System.out.println("Element with id: " + id + " not found");
+        }else {
+            shop.remove(id);
+        }
+    }
 }
--- src/test/java/NotDondExceptTest.java
+++ src/test/java/NotDondExceptTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import org.example.NotFoundException;
+import org.example.Product;
+import org.example.ShopRepository;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ShopRepository {
+public class NotDondExceptTest {
+    @Test
+    void removeProduct() {
+        ShopRepository shop = new ShopRepository();
+        Product product = new Product(0,"0", 0);
+        product.setPrice(1);
+        product.setTitle("1");
+        shop.add(product);
+        shop.remove(product.getId());
+        assertEquals(0, shop.findAll().length);
+    }
+
+    @Test
+    void removeNotExistProduct() {
+        ShopRepository shop = new ShopRepository();
+        Product product = new Product(0,"0", 0);
+        shop.add(product);
+        NotFoundException.removeById(shop ,1);
+        assertEquals(1, shop.findAll().length);
+    }
 }
